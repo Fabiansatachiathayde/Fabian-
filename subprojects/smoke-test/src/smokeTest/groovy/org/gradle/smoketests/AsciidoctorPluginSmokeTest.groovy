1cde5622dc89b1455a071049887eafb03947445d
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-03 02:14:29 -0500

    Initialize tt/90/update-asciidoctor-remove-main-from-javaexecspec

---
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AsciidoctorPluginSmokeTest.groovy
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.smoketests
+
+import org.gradle.integtests.fixtures.ToBeFixedForConfigurationCache
+
+class AsciidoctorPluginSmokeTest extends AbstractPluginValidatingSmokeTest {
+    @ToBeFixedForConfigurationCache(because = "Task.getProject() during execution")
+    def 'asciidoctor plugin #version'() {
+        given:
+        buildFile << """
+            plugins {
+                id 'org.asciidoctor.jvm.convert' version '${version}'
+            }
+
+            ${mavenCentralRepository()}
+        """
+
+        file('src/docs/asciidoc/test.adoc') << """
+            = Line Break Doc Title
+            :hardbreaks:
+
+            Rubies are red,
+            Topazes are blue.
+            """.stripIndent()
+
+        when:
+        runner('asciidoc').build()
+
+        then:
+        file('build/docs/asciidoc').isDirectory()
+
+        where:
+        version << TestedVersions.asciidoctor
+    }
+
+    @Override
+    Map<String, Versions> getPluginsToValidate() {
+        TestedVersions.asciidoctor.collectEntries([:]) { version ->
+            [
+                "org.asciidoctor.decktape",
+                "org.asciidoctor.editorconfig",
+                "org.asciidoctor.js.convert",
+                "org.asciidoctor.jvm.convert",
+                "org.asciidoctor.jvm.epub",
+                "org.asciidoctor.jvm.gems",
+                "org.asciidoctor.jvm.leanpub",
+                "org.asciidoctor.jvm.leanpub.dropbox-copy",
+                "org.asciidoctor.jvm.pdf",
+                "org.asciidoctor.jvm.revealjs",
+            ].collectEntries { plugin ->
+                [(plugin): Versions.of(version)]
+            }
+        }
+    }
+
+    @Override
+    void configureValidation(String pluginId, String version) {
+        validatePlugins {
+            alwaysPasses()
+        }
+    }
+}
