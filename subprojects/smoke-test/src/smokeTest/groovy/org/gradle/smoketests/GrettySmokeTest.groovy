cbf1013efbce4b94e16ea7ed2b31cf7dfd895f28
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-03 01:18:34 -0500

    Initialize lptr/execution/improve-property-visiting-2

---
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/GrettySmokeTest.groovy
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.smoketests
+
+import org.gradle.api.JavaVersion
+import org.gradle.integtests.fixtures.UnsupportedWithConfigurationCache
+import org.gradle.util.GradleVersion
+import org.gradle.util.internal.VersionNumber
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+@UnsupportedWithConfigurationCache(
+    because = "The Gretty plugin does not support configuration caching"
+)
+class GrettySmokeTest extends AbstractPluginValidatingSmokeTest {
+
+    def 'run Jetty with Gretty #grettyConfig.version'() {
+        given:
+        def grettyVersion = VersionNumber.parse(grettyConfig.version)
+        useSample('gretty-example')
+        buildFile << """
+            plugins {
+                id "war"
+                id "org.gretty" version "${grettyVersion}"
+            }
+
+            ${jcenterRepository()}
+
+            dependencies {
+                implementation group: 'log4j', name: 'log4j', version: '1.2.15', ext: 'jar'
+            }
+
+            gretty {
+                contextPath = 'quickstart'
+
+                httpPort = new ServerSocket(0).withCloseable { socket -> socket.getLocalPort() }
+                integrationTestTask = 'checkContainerUp'
+                servletContainer = '${grettyConfig.servletContainer}'
+            }
+
+            task checkContainerUp {
+                doLast {
+                    URL url = new URL("http://localhost:\${gretty.httpPort}/quickstart")
+                    assert url.text.contains('hello Gradle')
+                }
+            }
+        """
+
+        when:
+        def result = runner('checkContainerUp')
+            .expectDeprecationWarningIf(
+                grettyVersion < new VersionNumber(4, 0, 0, null),
+                "Internal API DependencyFactory.ClassPathNotation has been deprecated. This is scheduled to be removed in Gradle 8.0. Please use an appropriate call to DependencyHandler instead. Consult the upgrading guide for further information: https://docs.gradle.org/${GradleVersion.current().version}/userguide/upgrading_version_7.html#dependency_factory_renamed",
+                "https://github.com/gretty-gradle-plugin/gretty/pull/263"
+            )
+            .build()
+
+        then:
+        result.task(':checkContainerUp').outcome == SUCCESS
+
+        where:
+        grettyConfig << grettyConfigForCurrentJavaVersion()
+    }
+
+    @Override
+    Map<String, Versions> getPluginsToValidate() {
+        [
+            'org.gretty': Versions.of(grettyConfigForCurrentJavaVersion().collect { it.version } as String[])
+        ]
+    }
+
+    static def grettyConfigForCurrentJavaVersion() {
+        TestedVersions.gretty.findAll { JavaVersion.current().isCompatibleWith(it.javaMinVersion as JavaVersion) }
+    }
+}
