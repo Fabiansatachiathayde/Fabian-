e64ddeeba790fa0caa791959287608c5d18f7cab
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-03 01:10:30 -0500

    Initialize km/groovy-10521

---
+++ subprojects/integ-test/src/integTest/groovy/org/gradle/integtests/IsolatedAntBuilderMemoryLeakIntegrationTest.groovy
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.integtests
+
+import org.gradle.integtests.fixtures.AbstractIntegrationSpec
+import org.gradle.test.fixtures.file.TestFile
+import org.gradle.util.Requires
+import org.gradle.util.TestPrecondition
+
+class IsolatedAntBuilderMemoryLeakIntegrationTest extends AbstractIntegrationSpec {
+
+    private void goodCode(String groovyVersion, TestFile root = testDirectory) {
+        root.file("src/main/java/org/gradle/Class0.java") << "package org.gradle; public class Class0 { }"
+        root.file("src/main/groovy/org/gradle/Class1.groovy") << "package org.gradle; class Class1 { }"
+        buildFile << """
+
+            allprojects {
+                ${mavenCentralRepository()}
+            }
+
+            allprojects {
+                apply plugin: 'groovy'
+
+                dependencies {
+                    implementation $groovyVersion
+                }
+            }
+        """
+    }
+
+    private void withCodenarc(String groovyVersion, TestFile root = testDirectory) {
+        root.file("config/codenarc/rulesets.groovy") << """
+            ruleset {
+                ruleset('rulesets/naming.xml')
+            }
+        """
+        buildFile << """
+            allprojects {
+                apply plugin: 'codenarc'
+
+                dependencies {
+                    codenarc('org.codenarc:CodeNarc-Groovy4:3.1.0') {
+                        exclude group: 'org.apache.groovy'
+                    }
+                    codenarc $groovyVersion
+                }
+
+                codenarc {
+                    configFile = file('config/codenarc/rulesets.groovy')
+                }
+
+            }
+        """
+    }
+
+    private void withCheckstyle(TestFile root = testDirectory) {
+        root.file("config/checkstyle/checkstyle.xml") << """<!DOCTYPE module PUBLIC
+                    "-//Puppy Crawl//DTD Check Configuration 1.2//EN"
+                    "http://www.puppycrawl.com/dtds/configuration_1_2.dtd">
+            <module name="Checker">
+            </module>
+        """
+        buildFile << """
+            allprojects {
+                apply plugin: 'checkstyle'
+            }
+        """
+    }
+
+    void 'CodeNarc does not fail with PermGen space error'() {
+        given:
+        withCodenarc(groovyVersion)
+        withCheckstyle()
+        goodCode(groovyVersion)
+
+        expect:
+        succeeds 'check'
+
+        where:
+        groovyVersion << groovyVersions() * 3
+    }
+
+    private static List<String> groovyVersions() {
+        return [
+            'localGroovy()' // TODO as of July 2022, we can only test org.codenarc:CodeNarc-Groovy4:3.1.0 with Groovy 4
+        ]
+    }
+
+    @Requires(TestPrecondition.JDK11_OR_LATER) // grgit 5 requires JDK 11, see https://github.com/ajoberstar/grgit/issues/355
+    void "does not fail with a PermGen space error or a missing method exception"() {
+        given:
+        initGitDir()
+        buildFile << """
+            buildscript {
+              repositories {
+                ${mavenCentralRepository()}
+              }
+
+              dependencies {
+                classpath "org.ajoberstar.grgit:grgit-core:5.0.0"
+              }
+            }
+
+            import org.ajoberstar.grgit.*
+            Grgit.open(currentDir: project.rootProject.rootDir)
+        """
+        withCheckstyle()
+        goodCode('localGroovy()')
+
+        expect:
+        succeeds 'check'
+
+        where:
+        iteration << (0..10)
+    }
+}
