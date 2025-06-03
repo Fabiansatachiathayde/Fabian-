b7d0b04c72819b9665944a084fc6c2e9ac50aebb
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-03 00:44:43 -0500

    Initialize asodja/compilation-after-failure-2

---
+++ subprojects/language-java/src/main/java/org/gradle/api/internal/tasks/compile/incremental/recomp/AbstractRecompilationSpecProvider.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.tasks.compile.incremental.recomp;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableSet;
+import org.gradle.api.file.FileTree;
+import org.gradle.api.file.FileType;
+import org.gradle.api.internal.file.FileOperations;
+import org.gradle.api.internal.tasks.compile.CompilationFailedException;
+import org.gradle.api.internal.tasks.compile.JavaCompileSpec;
+import org.gradle.api.internal.tasks.compile.incremental.compilerapi.deps.DependentsSet;
+import org.gradle.api.internal.tasks.compile.incremental.compilerapi.deps.GeneratedResource;
+import org.gradle.api.internal.tasks.compile.incremental.transaction.CompileTransaction;
+import org.gradle.api.tasks.util.PatternSet;
+import org.gradle.internal.file.Deleter;
+import org.gradle.work.FileChange;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import static org.gradle.api.internal.tasks.compile.incremental.compilerapi.deps.GeneratedResource.Location.CLASS_OUTPUT;
+import static org.gradle.api.internal.tasks.compile.incremental.compilerapi.deps.GeneratedResource.Location.SOURCE_OUTPUT;
+
+abstract class AbstractRecompilationSpecProvider implements RecompilationSpecProvider {
+    private final Deleter deleter;
+    private final FileOperations fileOperations;
+    private final FileTree sourceTree;
+    private final Iterable<FileChange> sourceChanges;
+    private final boolean incremental;
+
+    public AbstractRecompilationSpecProvider(
+        Deleter deleter,
+        FileOperations fileOperations,
+        FileTree sourceTree,
+        Iterable<FileChange> sourceChanges,
+        boolean incremental
+    ) {
+        this.deleter = deleter;
+        this.fileOperations = fileOperations;
+        this.sourceTree = sourceTree;
+        this.sourceChanges = sourceChanges;
+        this.incremental = incremental;
+    }
+
+    @Override
+    public RecompilationSpec provideRecompilationSpec(CurrentCompilation current, PreviousCompilation previous) {
+        RecompilationSpec spec = new RecompilationSpec(previous);
+        SourceFileClassNameConverter sourceFileClassNameConverter = getSourceFileClassNameConverter(previous);
+
+        processClasspathChanges(current, previous, spec);
+        processOtherChanges(current, previous, spec, sourceFileClassNameConverter);
+        spec.addClassesToProcess(previous.getTypesToReprocess(spec.getClassesToCompile()));
+        return spec;
+    }
+
+    @Override
+    public CompileTransaction initCompilationSpecAndTransaction(JavaCompileSpec spec, RecompilationSpec recompilationSpec) {
+        if (!recompilationSpec.isBuildNeeded()) {
+            File tempDir = newCompileTransactionTempDir(spec);
+            spec.setSourceFiles(ImmutableSet.of());
+            spec.setClasses(Collections.emptySet());
+            return CompileTransaction.newTransaction(tempDir, fileOperations, deleter);
+        }
+
+        PatternSet classesToDelete = fileOperations.patternSet();
+        PatternSet sourceToCompile = fileOperations.patternSet();
+
+        SourceFileClassNameConverter sourceFileClassNameConverter = getSourceFileClassNameConverter(recompilationSpec.getPreviousCompilation());
+        prepareFilePatterns(recompilationSpec.getClassesToCompile(), classesToDelete, sourceToCompile, sourceFileClassNameConverter);
+        spec.setSourceFiles(narrowDownSourcesToCompile(sourceTree, sourceToCompile));
+        includePreviousCompilationOutputOnClasspath(spec);
+        addClassesToProcess(spec, recompilationSpec);
+        Map<GeneratedResource.Location, PatternSet> resourcesToDelete = prepareResourcePatterns(recompilationSpec.getResourcesToGenerate(), fileOperations);
+
+        File tempDir = newCompileTransactionTempDir(spec);
+        File compileDestinationDir = spec.getDestinationDir();
+        File annProcessorGeneratedSourcesDir = spec.getCompileOptions().getAnnotationProcessorGeneratedSourcesDirectory();
+        File headerOutputDir = spec.getCompileOptions().getHeaderOutputDirectory();
+        return CompileTransaction.newTransaction(tempDir, fileOperations, deleter)
+            .onFailureRollbackStashIfException(t -> t instanceof CompilationFailedException)
+            // Move files that should be deleted to new folders that will be reverted in case of a failure
+            .newTransactionalDirectory(dir -> dir.stashFilesForRollbackOnFailure(classesToDelete, compileDestinationDir))
+            .newTransactionalDirectory(dir -> dir.stashFilesForRollbackOnFailure(classesToDelete, annProcessorGeneratedSourcesDir))
+            .newTransactionalDirectory(dir -> dir.stashFilesForRollbackOnFailure(classesToDelete, headerOutputDir))
+            .newTransactionalDirectory(dir -> dir.stashFilesForRollbackOnFailure(resourcesToDelete.get(CLASS_OUTPUT), compileDestinationDir))
+            // If the client has not set a location for SOURCE_OUTPUT, javac outputs those files to the CLASS_OUTPUT directory, so delete that instead.
+            .newTransactionalDirectory(dir -> dir.stashFilesForRollbackOnFailure(resourcesToDelete.get(SOURCE_OUTPUT), MoreObjects.firstNonNull(annProcessorGeneratedSourcesDir, compileDestinationDir)))
+            // In the same situation with NATIVE_HEADER_OUTPUT, javac just NPEs.  Don't bother.
+            .newTransactionalDirectory(dir -> dir.stashFilesForRollbackOnFailure(resourcesToDelete.get(GeneratedResource.Location.NATIVE_HEADER_OUTPUT), headerOutputDir))
+            // Set compile outputs to new folders and in case of a success move content to original folders
+            .newTransactionalDirectory(dir -> dir
+                .onSuccessMoveFilesTo(compileDestinationDir)
+                .ensureEmptyKeepingDirectoriesFrom(compileDestinationDir)
+                .beforeExecutionDo(() -> spec.setDestinationDir(dir.getAsFile()))
+                .afterExecutionAlwaysDo(() -> spec.setDestinationDir(compileDestinationDir))
+            )
+            .newTransactionalDirectory(annProcessorGeneratedSourcesDir != null, dir -> dir
+                .onSuccessMoveFilesTo(annProcessorGeneratedSourcesDir)
+                .ensureEmptyKeepingDirectoriesFrom(annProcessorGeneratedSourcesDir)
+                .beforeExecutionDo(() -> spec.getCompileOptions().setAnnotationProcessorGeneratedSourcesDirectory(dir.getAsFile()))
+                .afterExecutionAlwaysDo(() -> spec.getCompileOptions().setAnnotationProcessorGeneratedSourcesDirectory(annProcessorGeneratedSourcesDir))
+            )
+            .newTransactionalDirectory(headerOutputDir != null, dir -> dir
+                .onSuccessMoveFilesTo(headerOutputDir)
+                .ensureEmptyKeepingDirectoriesFrom(headerOutputDir)
+                .beforeExecutionDo(() -> spec.getCompileOptions().setHeaderOutputDirectory(dir.getAsFile()))
+                .afterExecutionAlwaysDo(() -> spec.getCompileOptions().setHeaderOutputDirectory(headerOutputDir))
+            );
+    }
+
+    private File newCompileTransactionTempDir(JavaCompileSpec spec) {
+        return new File(spec.getTempDir(), "compileTransaction");
+    }
+
+    private Iterable<File> narrowDownSourcesToCompile(FileTree sourceTree, PatternSet sourceToCompile) {
+        return sourceTree.matching(sourceToCompile);
+    }
+
+    private static Map<GeneratedResource.Location, PatternSet> prepareResourcePatterns(Collection<GeneratedResource> staleResources, FileOperations fileOperations) {
+        Map<GeneratedResource.Location, PatternSet> resourcesByLocation = new EnumMap<>(GeneratedResource.Location.class);
+        for (GeneratedResource.Location location : GeneratedResource.Location.values()) {
+            resourcesByLocation.put(location, fileOperations.patternSet());
+        }
+        for (GeneratedResource resource : staleResources) {
+            resourcesByLocation.get(resource.getLocation()).include(resource.getPath());
+        }
+        return resourcesByLocation;
+    }
+
+    private void processOtherChanges(CurrentCompilation current, PreviousCompilation previous, RecompilationSpec spec, SourceFileClassNameConverter sourceFileClassNameConverter) {
+        if (spec.isFullRebuildNeeded()) {
+            return;
+        }
+        SourceFileChangeProcessor sourceFileChangeProcessor = new SourceFileChangeProcessor(previous);
+        for (FileChange fileChange : sourceChanges) {
+            if (spec.isFullRebuildNeeded()) {
+                return;
+            }
+            if (fileChange.getFileType() != FileType.FILE) {
+                continue;
+            }
+
+            String relativeFilePath = fileChange.getNormalizedPath();
+            Set<String> changedClasses = sourceFileClassNameConverter.getClassNames(relativeFilePath);
+            if (changedClasses.isEmpty() && !isIncrementalOnResourceChanges(current)) {
+                spec.setFullRebuildCause(rebuildClauseForChangedNonSourceFile(fileChange));
+            }
+            sourceFileChangeProcessor.processChange(changedClasses, spec);
+        }
+    }
+
+    protected abstract boolean isIncrementalOnResourceChanges(CurrentCompilation currentCompilation);
+
+    private void prepareFilePatterns(Collection<String> staleClasses, PatternSet filesToDelete, PatternSet sourceToCompile, SourceFileClassNameConverter sourceFileClassNameConverter) {
+        for (String staleClass : staleClasses) {
+            for (String sourcePath : sourceFileClassNameConverter.getRelativeSourcePaths(staleClass)) {
+                filesToDelete.include(sourcePath);
+                sourceToCompile.include(sourcePath);
+            }
+            filesToDelete.include(staleClass.replaceAll("\\.", "/").concat(".class"));
+            filesToDelete.include(staleClass.replaceAll("[.$]", "_").concat(".h"));
+        }
+    }
+
+    private void processClasspathChanges(CurrentCompilation current, PreviousCompilation previous, RecompilationSpec spec) {
+        DependentsSet dependents = current.findDependentsOfClasspathChanges(previous);
+        if (dependents.isDependencyToAll()) {
+            spec.setFullRebuildCause(dependents.getDescription());
+            return;
+        }
+        spec.addClassesToCompile(dependents.getPrivateDependentClasses());
+        spec.addClassesToCompile(dependents.getAccessibleDependentClasses());
+        spec.addResourcesToGenerate(dependents.getDependentResources());
+    }
+
+    private void addClassesToProcess(JavaCompileSpec spec, RecompilationSpec recompilationSpec) {
+        Set<String> classesToProcess = new HashSet<>(recompilationSpec.getClassesToProcess());
+        classesToProcess.removeAll(recompilationSpec.getClassesToCompile());
+        spec.setClasses(classesToProcess);
+    }
+
+    private void includePreviousCompilationOutputOnClasspath(JavaCompileSpec spec) {
+        List<File> classpath = new ArrayList<>(spec.getCompileClasspath());
+        File destinationDir = spec.getDestinationDir();
+        classpath.add(destinationDir);
+        spec.setCompileClasspath(classpath);
+    }
+
+    private String rebuildClauseForChangedNonSourceFile(FileChange fileChange) {
+        return String.format("%s '%s' has been %s", "resource", fileChange.getFile().getName(), fileChange.getChangeType().name().toLowerCase(Locale.US));
+    }
+
+    private SourceFileClassNameConverter getSourceFileClassNameConverter(PreviousCompilation previousCompilation) {
+        return new FileNameDerivingClassNameConverter(previousCompilation.getSourceToClassConverter(), getFileExtensions());
+    }
+
+    protected abstract Set<String> getFileExtensions();
+
+    @Override
+    public boolean isIncremental() {
+        return incremental;
+    }
+}
---
+++ subprojects/language-java/src/main/java/org/gradle/api/internal/tasks/compile/incremental/transaction/CompileTransaction.java
@@ -0,0 +1,326 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.tasks.compile.incremental.transaction;
+
+import org.gradle.api.UncheckedIOException;
+import org.gradle.api.internal.file.FileOperations;
+import org.gradle.api.tasks.WorkResult;
+import org.gradle.api.tasks.WorkResults;
+import org.gradle.api.tasks.util.PatternSet;
+import org.gradle.internal.file.Deleter;
+import org.gradle.language.base.internal.tasks.StaleOutputCleaner;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+/**
+ * A helper class to handle incremental compilation after failure: it makes moving files around easier and reverting state easier.
+ */
+public class CompileTransaction {
+
+    private final Deleter deleter;
+    private final FileOperations fileOperations;
+    private final File tempDir;
+    private final List<TransactionalDirectory> transactionalDirectories = new ArrayList<>();
+    private final AtomicInteger uniqueDirIdGenerator = new AtomicInteger();
+    private Predicate<Throwable> stashThrowablePredicate = t -> true;
+
+    private CompileTransaction(File tempDir, FileOperations fileOperations, Deleter deleter) {
+        this.tempDir = tempDir;
+        this.fileOperations = fileOperations;
+        this.deleter = deleter;
+    }
+
+    /**
+     * Creates a new transactional directory in a temp directory that can be used to move files to.
+     * Actual directory is created only when execute() method is called.
+     */
+    public CompileTransaction newTransactionalDirectory(Consumer<TransactionalDirectory> directoryConsumer) {
+        File directory = new File(tempDir, "dir-uniqueId" + uniqueDirIdGenerator.getAndIncrement());
+        TransactionalDirectory transactionalDirectory = new TransactionalDirectory(directory, fileOperations, deleter);
+        directoryConsumer.accept(transactionalDirectory);
+        transactionalDirectories.add(transactionalDirectory);
+        return this;
+    }
+
+    /**
+     * Creates and registers transactional directory only if condition is met. If it is not met directory will not be register. Also, any action set on directory won't run.
+     */
+    public CompileTransaction newTransactionalDirectory(boolean registerCondition, Consumer<TransactionalDirectory> directoryConsumer) {
+        return registerCondition ? newTransactionalDirectory(directoryConsumer) : this;
+    }
+
+    public CompileTransaction onFailureRollbackStashIfException(Predicate<Throwable> predicate) {
+        stashThrowablePredicate = checkNotNull(predicate);
+        return this;
+    }
+
+    /**
+     * Executes the function that is wrapped in the transaction. Function accepts a work result,
+     * that has a result of a stash operation. If some files were stashed, then work will be marked as "did work".
+     *
+     * Execute will always clean temp directory, so it is empty before execution.
+     */
+    public <T> T execute(Function<WorkResult, T> function) {
+        ensureEmptyDirectoriesBeforeAll();
+        WorkResult workResult = stashFilesBeforeExecution();
+        try {
+            transactionalDirectories.forEach(dir -> dir.beforeExecutionAction.run());
+            T result = function.apply(workResult);
+            transactionalDirectories.forEach(dir -> dir.onSuccessStashAction.run());
+            transactionalDirectories.forEach(dir -> dir.onSuccessMoveAction.run());
+            return result;
+        } catch (Throwable t) {
+            if (stashThrowablePredicate.test(t)) {
+                transactionalDirectories.forEach(dir -> dir.stashRollbackAction.run());
+            }
+            throw t;
+        } finally {
+            transactionalDirectories.forEach(dir -> dir.afterExecutionAlwaysDoAction.run());
+        }
+    }
+
+    private WorkResult stashFilesBeforeExecution() {
+        boolean didWork = false;
+        for (TransactionalDirectory directory : transactionalDirectories) {
+            didWork |= directory.stashFilesAction.getAsBoolean();
+        }
+        return WorkResults.didWork(didWork);
+    }
+
+    private void ensureEmptyDirectoriesBeforeAll() {
+        try {
+//            deleter.ensureEmptyDirectory(tempDir);
+            for (TransactionalDirectory directory : transactionalDirectories) {
+                if (directory.getAsFile().exists() && directory.keepFolderStructure != null) {
+                    Path currentDir = directory.getAsFile().toPath();
+                    try (Stream<Path> dirStream = Files.walk(currentDir)) {
+                        dirStream
+                            .map(Path::toFile)
+                            .sorted(Comparator.reverseOrder())
+                            .filter(it -> {
+                                if (it.isFile()) {
+                                    return true;
+                                }
+                                Path relativePath = currentDir.relativize(it.toPath());
+                                File newFile = new File(directory.keepFolderStructure, relativePath.toString());
+                                return !newFile.exists();
+                            })
+                            .forEach(File::delete);
+                    }
+                } else if (directory.isCreateDirectoryBeforeExecution) {
+                    deleter.ensureEmptyDirectory(directory.getAsFile());
+                }
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * Creates new transaction.
+     *
+     * @param tempDir a temporary directory that will be used the create transactional directories, it will always be cleaned before transaction execution.
+     * @param fileOperations file operations for move operations.
+     * @param deleter for delete operations.
+     */
+    public static CompileTransaction newTransaction(File tempDir, FileOperations fileOperations, Deleter deleter) {
+        return new CompileTransaction(tempDir, fileOperations, deleter);
+    }
+
+    public static class TransactionalDirectory {
+
+        private final String uniqueDirectoryId;
+        private final Deleter deleter;
+        private final FileOperations fileOperations;
+        private File directory;
+        private BooleanSupplier stashFilesAction = () -> false;
+        private Runnable stashRollbackAction = () -> {};
+        private Runnable onSuccessStashAction = () -> {};
+        private Runnable onSuccessMoveAction = () -> {};
+        private Runnable beforeExecutionAction = () -> {};
+        private Runnable afterExecutionAlwaysDoAction = () -> {};
+        private boolean isCreateDirectoryBeforeExecution;
+        private File keepFolderStructure;
+
+        public TransactionalDirectory(File directory, FileOperations fileOperations, Deleter deleter) {
+            this.uniqueDirectoryId = directory.getName();
+            this.directory = directory;
+            this.fileOperations = fileOperations;
+            this.deleter = deleter;
+        }
+
+        /**
+         * Stash a pattern set of files from source directory to this directory. Files are moved before execution and on failure rolled back.
+         */
+        public TransactionalDirectory stashFilesForRollbackOnFailure(PatternSet patternSet, File sourceDirectory) {
+            if (patternSet == null || patternSet.isEmpty() || sourceDirectory == null || !sourceDirectory.exists()) {
+                return this;
+            }
+
+            ensureEmptyKeepingDirectoriesFrom(sourceDirectory);
+            if (!hasNamePrefix()) {
+                withNamePrefix("stash-for-" + sourceDirectory.getName());
+            }
+
+            // Create stash function first
+            AtomicReference<Set<File>> fRef = new AtomicReference<>();
+            Set<File> newFiles = new HashSet<>();
+            stashFilesAction = () -> {
+                Set<File> files = fileOperations.fileTree(sourceDirectory).matching(patternSet).getFiles();
+                fRef.set(files);
+                moveFilesFromDirectoryTo(files, sourceDirectory, directory, newFiles::add);
+                return !newFiles.isEmpty();
+            };
+
+            onSuccessStashAction = () -> {
+                Set<File> parents = fRef.get().stream()
+                    .map(File::getParentFile)
+                    .collect(Collectors.toSet());
+                StaleOutputCleaner.cleanOutputDirectories(deleter, parents, sourceDirectory);
+            };
+
+            // Then create also unstash function
+            stashRollbackAction = () -> moveFilesFromDirectoryTo(newFiles, directory, sourceDirectory, file -> {});
+            return this;
+        }
+
+        /**
+         * Moves files from this directory to destination directory, but only on success. Any file that already exists in destination directory is replaced.
+         * Folder hierarchy is automatically created.
+         */
+        public TransactionalDirectory onSuccessMoveFilesTo(File destinationDirectory) {
+            if (destinationDirectory == null) {
+                return this;
+            }
+
+            createDirectoryBeforeExecution();
+            if (!hasNamePrefix()) {
+                withNamePrefix("out-for-" + destinationDirectory.getName());
+            }
+
+            onSuccessMoveAction = () -> moveAllFilesFromDirectoryTo(directory, destinationDirectory);
+            return this;
+        }
+
+        /**
+         * Sets an action that will be run just before execution.
+         */
+        public TransactionalDirectory beforeExecutionDo(Runnable runnable) {
+            beforeExecutionAction = runnable;
+            return this;
+        }
+
+        /**
+         * Sets an action that will be run after execution always, even if there is a failure.
+         */
+        public TransactionalDirectory afterExecutionAlwaysDo(Runnable runnable) {
+            afterExecutionAlwaysDoAction = runnable;
+            return this;
+        }
+
+        /**
+         * Add additional prefix to the folder. This is optional, but it makes it easier to debug.
+         * If stash or move files operation are used with valid directories, prefix is automatically set to 'stash-of-${dir.name}' or 'out-of-${dir.name} if not set manually.
+         */
+        public TransactionalDirectory withNamePrefix(String namePrefix) {
+            this.directory = new File(directory.getParentFile(), namePrefix + "-" + uniqueDirectoryId);
+            return this;
+        }
+
+        private boolean hasNamePrefix() {
+            return !directory.getName().equals(uniqueDirectoryId);
+        }
+
+        /**
+         * A flag that tells that a directory on disk should be created before execution.
+         * If stash or move files operation are used with valid directories, this is set to true by default.
+         */
+        public TransactionalDirectory createDirectoryBeforeExecution() {
+            this.isCreateDirectoryBeforeExecution = true;
+            return this;
+        }
+
+        public TransactionalDirectory ensureEmptyKeepingDirectoriesFrom(File file) {
+            this.keepFolderStructure = file;
+            return this;
+        }
+
+        public File getAsFile() {
+            return directory;
+        }
+
+        private void moveFilesFromDirectoryTo(Set<File> files, File sourceDirectory, File destinationDirectory, Consumer<File> newFileCollector) {
+            if (files.isEmpty() || sourceDirectory == null) {
+                return;
+            }
+
+            for (File file : files) {
+                Path relativePath = sourceDirectory.toPath().relativize(file.toPath());
+                File newFile = new File(destinationDirectory, relativePath.toString());
+                moveFile(file, newFile);
+                newFileCollector.accept(newFile);
+            }
+        }
+
+        private void moveAllFilesFromDirectoryTo(File sourceDirectory, File destinationDirectory) {
+            Path sourcePath = sourceDirectory.toPath();
+            try (Stream<Path> dirStream = Files.walk(sourcePath)) {
+                dirStream.filter(Files::isRegularFile)
+                    .forEach(path -> {
+                        File newFile = new File(destinationDirectory, sourcePath.relativize(path).toString());
+                        moveFile(path.toFile(), newFile);
+                    });
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        /**
+         * Moves file to the destination file. It also creates all intermediate folders if they don't exist.
+         *
+         * Note: If the destination file already exists it replaced.
+         */
+        private boolean moveFile(File sourceFile, File destinationFile) {
+            try {
+                destinationFile.getParentFile().mkdirs();
+                Files.move(sourceFile.toPath(), destinationFile.toPath(), REPLACE_EXISTING);
+                return true;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+    }
+}
