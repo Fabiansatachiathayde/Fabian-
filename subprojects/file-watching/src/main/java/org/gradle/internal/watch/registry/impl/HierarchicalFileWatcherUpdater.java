8f005f5eaed6dd7389ba5773051427598c0177a6
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-02 23:19:38 -0500

    Initialize donat/snapcraft-pr-on-release

---
+++ subprojects/file-watching/src/main/java/org/gradle/internal/watch/registry/impl/HierarchicalFileWatcherUpdater.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.internal.watch.registry.impl;
+
+import net.rubygrapefruit.platform.file.FileWatcher;
+import org.gradle.internal.snapshot.FileSystemLocationSnapshot;
+import org.gradle.internal.snapshot.SnapshotHierarchy;
+import org.gradle.internal.watch.registry.FileWatcherProbeRegistry;
+import org.gradle.internal.watch.registry.FileWatcherUpdater;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ * Updater for hierarchical file watchers.
+ *
+ * For hierarchical watchers, we can use the registered watchable hierarchies as watched directories.
+ * Build root directories are always watchable hierarchies.
+ * Watching the build root directories is better since they are less likely to be deleted and
+ * nearly no changes to the watched directories are necessary when running builds on the same project.
+ *
+ * To allow deleting the build root directories, we need to stop watching a build root directory if there are no more snapshots in the VFS inside,
+ * since watched directories can't be deleted on Windows.
+ *
+ * The build root directories are discovered as included builds are encountered at the start of a build, and then they are removed when the build finishes.
+ *
+ * This is the lifecycle for the watchable hierarchies:
+ * - During a build, there will be various calls to {@link FileWatcherUpdater#registerWatchableHierarchy(File, SnapshotHierarchy)},
+ *   each call augmenting the collection. The watchers will be updated accordingly.
+ * - When updating the watches, we watch watchable hierarchies registered for this build or old watched directories from previous builds instead of
+ *   directories inside them.
+ * - At the end of the build
+ *   - stop watching the watchable directories with nothing to watch inside
+ *   - remember the currently watched directories as old watched directories for the next build
+ *   - remove everything that isn't watched from the virtual file system.
+ */
+public class HierarchicalFileWatcherUpdater extends AbstractFileWatcherUpdater {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HierarchicalFileWatcherUpdater.class);
+
+    private final FileWatcher fileWatcher;
+    private final MovedHierarchyHandler movedHierarchyHandler;
+
+    public HierarchicalFileWatcherUpdater(
+        FileWatcher fileWatcher,
+        FileSystemLocationToWatchValidator locationToWatchValidator,
+        FileWatcherProbeRegistry probeRegistry, WatchableHierarchies watchableHierarchies,
+        MovedHierarchyHandler movedHierarchyHandler
+    ) {
+        super(locationToWatchValidator, probeRegistry, watchableHierarchies);
+        this.fileWatcher = fileWatcher;
+        this.movedHierarchyHandler = movedHierarchyHandler;
+    }
+
+    @Override
+    protected boolean handleVirtualFileSystemContentsChanged(Collection<FileSystemLocationSnapshot> removedSnapshots, Collection<FileSystemLocationSnapshot> addedSnapshots, SnapshotHierarchy root) {
+        return watchableHierarchies.stream().anyMatch(watchableHierarchy -> {
+            boolean hasSnapshotsToWatch = root.hasDescendantsUnder(watchableHierarchy.getPath());
+            if (watchedFiles.contains(watchableHierarchy)) {
+                // Need to stop watching this hierarchy
+                return !hasSnapshotsToWatch;
+            } else {
+                // Need to start watching this hierarchy
+                return hasSnapshotsToWatch;
+            }
+        });
+    }
+
+    @Override
+    protected SnapshotHierarchy doUpdateVfsOnBuildStarted(SnapshotHierarchy root) {
+        return movedHierarchyHandler.handleMovedHierarchies(root);
+    }
+
+    @Override
+    protected void startWatchingHierarchies(Collection<File> hierarchiesToStartWatching) {
+        fileWatcher.startWatching(hierarchiesToStartWatching);
+    }
+
+    @Override
+    protected void stopWatchingHierarchies(Collection<File> hierarchiesToStopWatching) {
+        if (!fileWatcher.stopWatching(hierarchiesToStopWatching)) {
+            LOGGER.debug("Couldn't stop watching directories: {}", hierarchiesToStopWatching);
+        }
+    }
+
+    @Override
+    protected void startWatchingProbeForHierarchy(File hierarchyToWatch) {
+        // No need to start watching anything, probe directory is under the watched hierarchy
+    }
+
+    @Override
+    protected void stopWatchingProbeForHierarchy(File hierarchyToWatch) {
+        // No need to stop watching anything, probe directory is under the watched hierarchy
+    }
+
+    @Override
+    protected WatchableHierarchies.Invalidator createInvalidator() {
+        return (location, currentRoot) -> currentRoot.invalidate(location, SnapshotHierarchy.NodeDiffListener.NOOP);
+    }
+
+    public interface MovedHierarchyHandler {
+        /**
+         * On Windows when watched hierarchies are moved, the OS does not send a notification,
+         * even though the VFS should be updated. Our best bet here is to cull any moved watch
+         * roots from the VFS at the start of every build.
+         */
+        SnapshotHierarchy handleMovedHierarchies(SnapshotHierarchy root);
+    }
+}
---
+++ subprojects/plugins/src/main/java/org/gradle/api/plugins/jvm/internal/DefaultJvmTestSuite.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.plugins.jvm.internal;
+
+import com.google.common.base.Preconditions;
+import org.gradle.api.Action;
+import org.gradle.api.ExtensiblePolymorphicDomainObjectContainer;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.dsl.DependencyHandler;
+import org.gradle.api.internal.tasks.AbstractTaskDependency;
+import org.gradle.api.internal.tasks.TaskDependencyResolveContext;
+import org.gradle.api.internal.tasks.testing.filter.DefaultTestFilter;
+import org.gradle.api.internal.tasks.testing.junit.JUnitTestFramework;
+import org.gradle.api.internal.tasks.testing.junitplatform.JUnitPlatformTestFramework;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JvmTestSuitePlugin;
+import org.gradle.api.plugins.jvm.ComponentDependencies;
+import org.gradle.api.plugins.jvm.JvmTestSuite;
+import org.gradle.api.plugins.jvm.JvmTestSuiteTarget;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.TaskDependency;
+
+import javax.inject.Inject;
+
+public abstract class DefaultJvmTestSuite implements JvmTestSuite {
+    private enum Frameworks {
+        JUNIT4, JUNIT_JUPITER, NONE;
+    }
+    private static class TestingFramework {
+        private final Frameworks framework;
+        private final String version;
+
+        private TestingFramework(Frameworks framework, String version) {
+            Preconditions.checkNotNull(version);
+            this.framework = framework;
+            this.version = version;
+        }
+    }
+    private final static TestingFramework NO_OPINION = new TestingFramework(Frameworks.NONE, "unset");
+
+    private final ExtensiblePolymorphicDomainObjectContainer<JvmTestSuiteTarget> targets;
+    private final SourceSet sourceSet;
+    private final String name;
+    private final ComponentDependencies dependencies;
+    private boolean attachedDependencies;
+    private final Action<Void> attachDependencyAction;
+
+    protected abstract Property<TestingFramework> getTestingFramework();
+
+    @Inject
+    public DefaultJvmTestSuite(String name, ConfigurationContainer configurations, DependencyHandler dependencies, SourceSetContainer sourceSets) {
+        this.name = name;
+        this.sourceSet = sourceSets.create(getName());
+
+        Configuration compileOnly = configurations.getByName(sourceSet.getCompileOnlyConfigurationName());
+        Configuration implementation = configurations.getByName(sourceSet.getImplementationConfigurationName());
+        Configuration runtimeOnly = configurations.getByName(sourceSet.getRuntimeOnlyConfigurationName());
+
+        this.attachedDependencies = false;
+        // This complexity is to keep the built-in test suite from automatically adding dependencies
+        // unless a user explicitly calls one of the useXXX methods
+        // Eventually, we should deprecate this behavior and provide a way for users to opt out
+        // We could then always add these dependencies.
+        this.attachDependencyAction = x -> attachDependenciesForTestFramework(dependencies, implementation);
+
+        if (!name.equals(JvmTestSuitePlugin.DEFAULT_TEST_SUITE_NAME)) {
+            useJUnitJupiter();
+        } else {
+            // for the built-in test suite, we don't express an opinion, so we will not add any dependencies
+            // if a user explicitly calls useJUnit or useJUnitJupiter, the built-in test suite will behave like a custom one
+            // and add dependencies automatically.
+            getTestingFramework().convention(NO_OPINION);
+        }
+
+        this.targets = getObjectFactory().polymorphicDomainObjectContainer(JvmTestSuiteTarget.class);
+        this.targets.registerBinding(JvmTestSuiteTarget.class, DefaultJvmTestSuiteTarget.class);
+
+        this.dependencies = getObjectFactory().newInstance(DefaultComponentDependencies.class, implementation, compileOnly, runtimeOnly);
+
+        addDefaultTestTarget();
+
+        this.targets.withType(JvmTestSuiteTarget.class).configureEach(target -> {
+            target.getTestTask().configure(task -> {
+                task.getTestFrameworkProperty().convention(getTestingFramework().map(framework -> {
+                    switch(framework.framework) {
+                        case NONE:
+                        case JUNIT4:
+                            return new JUnitTestFramework(task, (DefaultTestFilter) task.getFilter());
+                        case JUNIT_JUPITER:
+                            return new JUnitPlatformTestFramework((DefaultTestFilter) task.getFilter());
+                        default:
+                            throw new IllegalStateException("do not know how to handle " + framework);
+                    }
+                }));
+            });
+        });
+
+    }
+
+    private void attachDependenciesForTestFramework(DependencyHandler dependencies, Configuration implementation) {
+        if (!attachedDependencies) {
+            dependencies.addProvider(implementation.getName(), getTestingFramework().map(framework -> {
+                switch (framework.framework) {
+                    case JUNIT4:
+                        return "junit:junit:" + framework.version;
+                    case JUNIT_JUPITER:
+                        return "org.junit.jupiter:junit-jupiter:" + framework.version;
+                    default:
+                        throw new IllegalStateException("do not know how to handle " + framework);
+                }
+            }));
+            attachedDependencies = true;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Inject
+    public abstract ObjectFactory getObjectFactory();
+
+    public SourceSet getSources() {
+        return sourceSet;
+    }
+    public void sources(Action<? super SourceSet> configuration) {
+        configuration.execute(getSources());
+    }
+
+    public ExtensiblePolymorphicDomainObjectContainer<JvmTestSuiteTarget> getTargets() {
+        return targets;
+    }
+
+    public void addDefaultTestTarget() {
+        final String target;
+        if (getName().equals(JvmTestSuitePlugin.DEFAULT_TEST_SUITE_NAME)) {
+            target = JavaPlugin.TEST_TASK_NAME;
+        } else {
+            target = getName(); // For now, we'll just name the test task for the single target for the suite with the suite name
+        }
+
+        targets.register(target);
+    }
+
+    @Override
+    public void useJUnit() {
+        useJUnit("4.13");
+    }
+
+    @Override
+    public void useJUnit(String version) {
+        setFrameworkTo(new TestingFramework(Frameworks.JUNIT4, version));
+    }
+
+    private void setFrameworkTo(TestingFramework framework) {
+        getTestingFramework().set(framework);
+        attachDependencyAction.execute(null);
+    }
+
+    @Override
+    public void useJUnitJupiter() {
+        useJUnitJupiter("5.7.1");
+    }
+
+    @Override
+    public void useJUnitJupiter(String version) {
+        setFrameworkTo(new TestingFramework(Frameworks.JUNIT_JUPITER, version));
+    }
+
+    @Override
+    public ComponentDependencies getDependencies() {
+        return dependencies;
+    }
+
+    @Override
+    public void dependencies(Action<? super ComponentDependencies> action) {
+        action.execute(dependencies);
+    }
+
+    @Override
+    public TaskDependency getBuildDependencies() {
+        return new AbstractTaskDependency() {
+            @Override
+            public void visitDependencies(TaskDependencyResolveContext context) {
+                getTargets().forEach(context::add);
+            }
+        };
+    }
+}
