0c06e13df4a77a01dbfb12749bc32067d83dfb2f
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-03 02:04:52 -0500

    Initialize jvandort/launcher/detangle-command-line-action-factory

---
+++ subprojects/core/src/main/java/org/gradle/api/internal/cache/DefaultCacheConfigurations.java
@@ -0,0 +1,261 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.cache;
+
+import org.gradle.api.Action;
+import org.gradle.api.cache.CacheResourceConfiguration;
+import org.gradle.api.cache.Cleanup;
+import org.gradle.api.internal.DocumentationRegistry;
+import org.gradle.api.internal.provider.DefaultProperty;
+import org.gradle.api.internal.provider.DefaultProvider;
+import org.gradle.api.internal.provider.PropertyHost;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.Provider;
+import org.gradle.cache.CleanupFrequency;
+import org.gradle.internal.Describables;
+import org.gradle.internal.DisplayName;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.inject.Inject;
+import java.util.function.Supplier;
+
+import static org.gradle.internal.time.TimestampSuppliers.daysAgo;
+
+abstract public class DefaultCacheConfigurations implements CacheConfigurationsInternal {
+    private static final DocumentationRegistry DOCUMENTATION_REGISTRY = new DocumentationRegistry();
+    private static final String RELEASED_WRAPPERS = "releasedWrappers";
+    private static final String SNAPSHOT_WRAPPERS = "snapshotWrappers";
+    private static final String DOWNLOADED_RESOURCES = "downloadedResources";
+    private static final String CREATED_RESOURCES = "createdResources";
+    static final String ILLEGAL_MODIFICATION_ERROR = "The property '%s' was modified from an unsafe location (for instance a settings script or plugin).  This property can only be changed in an init script, preferably stored in the init.d directory inside the Gradle user home directory. See " + DOCUMENTATION_REGISTRY.getDocumentationFor("directory_layout", "dir:gradle_user_home:configure_cache_cleanup") + " for more information.";
+
+    private final CacheResourceConfigurationInternal releasedWrappersConfiguration;
+    private final CacheResourceConfigurationInternal snapshotWrappersConfiguration;
+    private final CacheResourceConfigurationInternal downloadedResourcesConfiguration;
+    private final CacheResourceConfigurationInternal createdResourcesConfiguration;
+    private final UnlockableProperty<Cleanup> cleanup;
+
+    @Inject
+    public DefaultCacheConfigurations(ObjectFactory objectFactory, PropertyHost propertyHost) {
+        this.releasedWrappersConfiguration = createResourceConfiguration(objectFactory, RELEASED_WRAPPERS, DEFAULT_MAX_AGE_IN_DAYS_FOR_RELEASED_DISTS);
+        this.snapshotWrappersConfiguration = createResourceConfiguration(objectFactory, SNAPSHOT_WRAPPERS, DEFAULT_MAX_AGE_IN_DAYS_FOR_SNAPSHOT_DISTS);
+        this.downloadedResourcesConfiguration = createResourceConfiguration(objectFactory, DOWNLOADED_RESOURCES, DEFAULT_MAX_AGE_IN_DAYS_FOR_DOWNLOADED_CACHE_ENTRIES);
+        this.createdResourcesConfiguration = createResourceConfiguration(objectFactory, CREATED_RESOURCES, DEFAULT_MAX_AGE_IN_DAYS_FOR_CREATED_CACHE_ENTRIES);
+        this.cleanup = new DefaultUnlockableProperty<>(propertyHost, Cleanup.class, "cleanup").convention(Cleanup.DEFAULT);
+        lockValues();
+    }
+
+    private static CacheResourceConfigurationInternal createResourceConfiguration(ObjectFactory objectFactory, String name, int defaultDays) {
+        CacheResourceConfigurationInternal resourceConfiguration = objectFactory.newInstance(DefaultCacheResourceConfiguration.class, name);
+        resourceConfiguration.getRemoveUnusedEntriesOlderThan().convention(providerFromSupplier(daysAgo(defaultDays)));
+        return resourceConfiguration;
+    }
+
+    @Override
+    public void releasedWrappers(Action<? super CacheResourceConfiguration> cacheConfiguration) {
+        cacheConfiguration.execute(releasedWrappersConfiguration);
+    }
+
+    @Override
+    public CacheResourceConfigurationInternal getReleasedWrappers() {
+        return releasedWrappersConfiguration;
+    }
+
+    @Override
+    public void snapshotWrappers(Action<? super CacheResourceConfiguration> cacheConfiguration) {
+        cacheConfiguration.execute(snapshotWrappersConfiguration);
+    }
+
+    @Override
+    public CacheResourceConfigurationInternal getSnapshotWrappers() {
+        return snapshotWrappersConfiguration;
+    }
+
+    @Override
+    public void downloadedResources(Action<? super CacheResourceConfiguration> cacheConfiguration) {
+        cacheConfiguration.execute(downloadedResourcesConfiguration);
+    }
+
+    @Override
+    public CacheResourceConfigurationInternal getDownloadedResources() {
+        return downloadedResourcesConfiguration;
+    }
+
+    @Override
+    public void createdResources(Action<? super CacheResourceConfiguration> cacheConfiguration) {
+        cacheConfiguration.execute(createdResourcesConfiguration);
+    }
+
+    @Override
+    public CacheResourceConfigurationInternal getCreatedResources() {
+        return createdResourcesConfiguration;
+    }
+
+    @Override
+    public UnlockableProperty<Cleanup> getCleanup() {
+        return cleanup;
+    }
+
+    @Override
+    public Provider<CleanupFrequency> getCleanupFrequency() {
+        return getCleanup().map(cleanup -> ((CleanupInternal)cleanup).getCleanupFrequency());
+    }
+
+    @Override
+    public void withMutableValues(Runnable runnable) {
+        unlockValues();
+        try {
+            runnable.run();
+        } finally {
+            lockValues();
+        }
+    }
+
+    private void lockValues() {
+        releasedWrappersConfiguration.getRemoveUnusedEntriesOlderThan().lock();
+        snapshotWrappersConfiguration.getRemoveUnusedEntriesOlderThan().lock();
+        downloadedResourcesConfiguration.getRemoveUnusedEntriesOlderThan().lock();
+        createdResourcesConfiguration.getRemoveUnusedEntriesOlderThan().lock();
+        getCleanup().lock();
+    }
+
+    private void unlockValues() {
+        releasedWrappersConfiguration.getRemoveUnusedEntriesOlderThan().unlock();
+        snapshotWrappersConfiguration.getRemoveUnusedEntriesOlderThan().unlock();
+        downloadedResourcesConfiguration.getRemoveUnusedEntriesOlderThan().unlock();
+        createdResourcesConfiguration.getRemoveUnusedEntriesOlderThan().unlock();
+        getCleanup().unlock();
+    }
+
+    private static <T> Provider<T> providerFromSupplier(Supplier<T> supplier) {
+        return new DefaultProvider<>(supplier::get);
+    }
+
+    static abstract class DefaultCacheResourceConfiguration implements CacheResourceConfigurationInternal {
+        private final String name;
+        private final UnlockableProperty<Long> removeUnusedEntriesOlderThan;
+
+        @Inject
+        public DefaultCacheResourceConfiguration(PropertyHost propertyHost, String name) {
+            this.name = name;
+            this.removeUnusedEntriesOlderThan = new DefaultUnlockableProperty<>(propertyHost, Long.class, "removeUnusedEntriesOlderThan");
+        }
+
+        @Override
+        public UnlockableProperty<Long> getRemoveUnusedEntriesOlderThan() {
+            return removeUnusedEntriesOlderThan;
+        }
+
+        /**
+         * @implNote Returns a supplier mapped from the property.  This provides a supplier that is resilient
+         * to subsequent changes to the property value as opposed to just calling get() on the property.
+         */
+        @Override
+        public Supplier<Long> getRemoveUnusedEntriesOlderThanAsSupplier() {
+            return () -> getRemoveUnusedEntriesOlderThan().get();
+        }
+
+
+        @Override
+        public void setRemoveUnusedEntriesAfterDays(int removeUnusedEntriesAfterDays) {
+            if (removeUnusedEntriesAfterDays < 1) {
+                throw new IllegalArgumentException(name + " cannot be set to retain entries for " + removeUnusedEntriesAfterDays + " days.  For time frames shorter than one day, use the 'removeUnusedEntriesOlderThan' property.");
+            }
+            getRemoveUnusedEntriesOlderThan().set(providerFromSupplier(daysAgo(removeUnusedEntriesAfterDays)));
+        }
+    }
+
+    /**
+     * A non-threadsafe implementation of {@link org.gradle.api.internal.cache.CacheConfigurationsInternal.UnlockableProperty}.
+     */
+    @NotThreadSafe
+    static class DefaultUnlockableProperty<T> extends DefaultProperty<T> implements UnlockableProperty<T> {
+        private final String displayName;
+        private boolean mutable = true;
+
+        public DefaultUnlockableProperty(PropertyHost propertyHost, Class<T> type, String displayName) {
+            super(propertyHost, type);
+            this.displayName = displayName;
+        }
+
+        public void lock() {
+            mutable = false;
+        }
+
+        public void unlock() {
+            mutable = true;
+        }
+
+        private IllegalStateException lockedError() {
+            return new IllegalStateException(String.format(ILLEGAL_MODIFICATION_ERROR, getDisplayName()));
+        }
+
+        private void onlyIfMutable(Runnable runnable) {
+            if (mutable) {
+                runnable.run();
+            } else {
+                throw lockedError();
+            }
+        }
+
+        @Override
+        @Nonnull
+        protected DisplayName getDisplayName() {
+            if (displayName != null) {
+                return Describables.of(displayName);
+            } else {
+                return super.getDisplayName();
+            }
+        }
+
+        @Override
+        public void set(@Nullable T value) {
+            onlyIfMutable(() -> super.set(value));
+        }
+
+        @Override
+        public void set(Provider<? extends T> provider) {
+            onlyIfMutable(() -> super.set(provider));
+        }
+
+        @Override
+        public DefaultUnlockableProperty<T> value(@Nullable T value) {
+            onlyIfMutable(() -> super.value(value));
+            return this;
+        }
+
+        @Override
+        public DefaultUnlockableProperty<T> value(Provider<? extends T> provider) {
+            onlyIfMutable(() -> super.value(provider));
+            return this;
+        }
+
+        @Override
+        public DefaultUnlockableProperty<T> convention(@Nullable T value) {
+            onlyIfMutable(() -> super.convention(value));
+            return this;
+        }
+
+        @Override
+        public DefaultUnlockableProperty<T> convention(Provider<? extends T> provider) {
+            onlyIfMutable(() -> super.convention(provider));
+            return this;
+        }
+    }
+}
---
+++ subprojects/launcher/src/main/java/org/gradle/launcher/cli/DefaultCommandLineActionFactory.java
@@ -0,0 +1,312 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.launcher.cli;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import org.apache.tools.ant.Main;
+import org.codehaus.groovy.util.ReleaseInfo;
+import org.gradle.api.Action;
+import org.gradle.api.launcher.cli.WelcomeMessageConfiguration;
+import org.gradle.api.launcher.cli.WelcomeMessageDisplayMode;
+import org.gradle.api.logging.configuration.LoggingConfiguration;
+import org.gradle.cli.CommandLineArgumentException;
+import org.gradle.cli.CommandLineParser;
+import org.gradle.cli.ParsedCommandLine;
+import org.gradle.configuration.GradleLauncherMetaData;
+import org.gradle.initialization.layout.BuildLayoutFactory;
+import org.gradle.internal.Actions;
+import org.gradle.internal.buildevents.BuildExceptionReporter;
+import org.gradle.internal.jvm.Jvm;
+import org.gradle.internal.logging.DefaultLoggingConfiguration;
+import org.gradle.internal.logging.LoggingConfigurationBuildOptions;
+import org.gradle.internal.logging.LoggingManagerInternal;
+import org.gradle.internal.logging.services.LoggingServiceRegistry;
+import org.gradle.internal.logging.text.StyledTextOutputFactory;
+import org.gradle.internal.os.OperatingSystem;
+import org.gradle.internal.service.ServiceRegistry;
+import org.gradle.launcher.bootstrap.CommandLineActionFactory;
+import org.gradle.launcher.bootstrap.ExecutionListener;
+import org.gradle.launcher.cli.converter.BuildLayoutConverter;
+import org.gradle.launcher.cli.converter.BuildOptionBackedConverter;
+import org.gradle.launcher.cli.converter.InitialPropertiesConverter;
+import org.gradle.launcher.cli.converter.LayoutToPropertiesConverter;
+import org.gradle.launcher.cli.converter.WelcomeMessageBuildOptions;
+import org.gradle.launcher.configuration.AllProperties;
+import org.gradle.launcher.configuration.BuildLayoutResult;
+import org.gradle.launcher.configuration.InitialProperties;
+import org.gradle.util.internal.DefaultGradleVersion;
+
+import javax.annotation.Nullable;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * <p>Responsible for converting a set of command-line arguments into an {@link Action}.</p>
+ */
+public class DefaultCommandLineActionFactory implements CommandLineActionFactory {
+    public static final String WELCOME_MESSAGE_ENABLED_SYSTEM_PROPERTY = "org.gradle.internal.launcher.welcomeMessageEnabled";
+    private static final String HELP = "h";
+    private static final String VERSION = "v";
+    private static final String VERSION_CONTINUE = "V";
+
+    private final LoggingServiceRegistry loggingServices;
+    private final Supplier<List<CommandLineActionCreator>> actionCreatorsFactory;
+
+    public DefaultCommandLineActionFactory() {
+        this(LoggingServiceRegistry.newCommandLineProcessLogging());
+    }
+
+    private DefaultCommandLineActionFactory(LoggingServiceRegistry loggingServices) {
+        this(loggingServices, () -> new BuildActionsFactory(loggingServices));
+    }
+
+    /**
+     * Allows tests to inject a mock build action factory to avoid attempting to execute actual builds.
+     */
+    @VisibleForTesting
+    public DefaultCommandLineActionFactory(LoggingServiceRegistry loggingServices, Supplier<CommandLineActionCreator> buildActionsFactory) {
+        this.loggingServices = loggingServices;
+        this.actionCreatorsFactory = () -> ImmutableList.of(new BuiltInActionCreator(), new ContinuingActionCreator(), buildActionsFactory.get());
+    }
+
+    @Override
+    public Action<ExecutionListener> convert(List<String> args) {
+        Action<ExecutionListener> action = new ParseAndBuildAction(args, actionCreatorsFactory);
+        return new WithLogging(loggingServices, args, new DefaultLoggingConfiguration(), action);
+    }
+
+    private static GradleLauncherMetaData clientMetaData() {
+        return new GradleLauncherMetaData();
+    }
+
+    private static void showUsage(PrintStream out, CommandLineParser parser) {
+        out.println();
+        out.print("USAGE: ");
+        clientMetaData().describeCommand(out, "[option...]", "[task...]");
+        out.println();
+        out.println();
+        parser.printUsage(out);
+        out.println();
+    }
+
+    private static class BuiltInActionCreator implements CommandLineActionCreator {
+        @Override
+        public void configureCommandLineParser(CommandLineParser parser) {
+            parser.option(HELP, "?", "help").hasDescription("Shows this help message.");
+            parser.option(VERSION, "version").hasDescription("Print version info and exit.");
+        }
+
+        @Override
+        @Nullable
+        public Action<? super ExecutionListener> createAction(CommandLineParser parser, ParsedCommandLine commandLine) {
+            if (commandLine.hasOption(HELP)) {
+                return listener -> showHelp(parser);
+            }
+            if (commandLine.hasOption(VERSION)) {
+                return DefaultCommandLineActionFactory::showVersion;
+            }
+            return null;
+        }
+    }
+
+    /**
+     * This {@link CommandLineActionCreator} is responsible for handling any command line options that produce {@link ContinuingAction}s.
+     */
+    private static class ContinuingActionCreator implements CommandLineActionCreator {
+        @Override
+        public void configureCommandLineParser(CommandLineParser parser) {
+            parser.option(VERSION_CONTINUE, "show-version").hasDescription("Print version info and continue.");
+        }
+
+        @Override
+        @Nullable
+        public ContinuingAction<? super ExecutionListener> createAction(CommandLineParser parser, ParsedCommandLine commandLine) {
+            if (commandLine.hasOption(DefaultCommandLineActionFactory.VERSION_CONTINUE)) {
+                return DefaultCommandLineActionFactory::showVersion;
+            }
+            return null;
+        }
+    }
+
+    private static void showCommandLineParseFailure(Exception exception, CommandLineParser parser) {
+        System.err.println();
+        System.err.println(exception.getMessage());
+        showUsage(System.err, parser);
+    }
+
+    private static void showHelp(CommandLineParser parser) {
+        System.out.println();
+        System.out.print("To see help contextual to the project, use ");
+        clientMetaData().describeCommand(System.out, "help");
+        System.out.println();
+        showUsage(System.out, parser);
+    }
+
+    private static void showVersion(ExecutionListener executionListener) {
+        DefaultGradleVersion currentVersion = DefaultGradleVersion.current();
+
+        final StringBuilder sb = new StringBuilder();
+        sb.append("%n------------------------------------------------------------%nGradle ");
+        sb.append(currentVersion.getVersion());
+        sb.append("%n------------------------------------------------------------%n%nBuild time:   ");
+        sb.append(currentVersion.getBuildTimestamp());
+        sb.append("%nRevision:     ");
+        sb.append(currentVersion.getGitRevision());
+        sb.append("%n%nKotlin:       ");
+        sb.append(KotlinDslVersion.current().getKotlinVersion());
+        sb.append("%nGroovy:       ");
+        sb.append(ReleaseInfo.getVersion());
+        sb.append("%nAnt:          ");
+        sb.append(Main.getAntVersion());
+        sb.append("%nJVM:          ");
+        sb.append(Jvm.current());
+        sb.append("%nOS:           ");
+        sb.append(OperatingSystem.current());
+        sb.append("%n");
+
+        System.out.println(String.format(sb.toString()));
+    }
+
+    /**
+     * An {@link Action} which executes the actions generated by list of provided {@link CommandLineActionCreator}s.
+     *
+     * The provided {@link CommandLineActionCreator}s each attempt to create an {@link Action} from the given CLI args.
+     * After processing each created action, subsequent actions are only executed if the processed action is a {@link ContinuingAction}.
+     */
+    private static class ParseAndBuildAction implements Action<ExecutionListener> {
+        private final List<String> args;
+        private final Supplier<List<CommandLineActionCreator>> actionCreatorsFactory;
+
+        private ParseAndBuildAction(List<String> args, Supplier<List<CommandLineActionCreator>> actionCreators) {
+            this.args = args;
+            this.actionCreatorsFactory = actionCreators;
+        }
+
+        @Override
+        public void execute(ExecutionListener executionListener) {
+            // We fetch the action creators lazily since BuildActionsFactory calls NativeServices.getInstance() in its constructor,
+            // but the NativeServices are not initialized until NativeServicesInitializingAction#execute is called.
+            List<CommandLineActionCreator> actionCreators = actionCreatorsFactory.get();
+
+            CommandLineParser parser = new CommandLineParser();
+            actionCreators.forEach(creator -> creator.configureCommandLineParser(parser));
+
+            try {
+                ParsedCommandLine commandLine = parser.parse(args);
+                combineActions(parser, commandLine, actionCreators).execute(executionListener);
+            } catch (CommandLineArgumentException e) {
+                showCommandLineParseFailure(e, parser);
+                executionListener.onFailure(e);
+            }
+        }
+
+        private static Action<? super ExecutionListener> combineActions(
+            CommandLineParser parser, ParsedCommandLine commandLine, List<CommandLineActionCreator> actionCreators
+        ) {
+            List<Action<? super ExecutionListener>> actions = new ArrayList<>(2);
+            for (CommandLineActionCreator actionCreator : actionCreators) {
+                Action<? super ExecutionListener> action = actionCreator.createAction(parser, commandLine);
+                if (action != null) {
+                    actions.add(action);
+                    if (!(action instanceof ContinuingAction)) {
+                        break;
+                    }
+                }
+            }
+
+            if (!actions.isEmpty()) {
+                return Actions.composite(actions);
+            }
+
+            throw new UnsupportedOperationException("No action factory for specified command-line arguments.");
+        }
+    }
+
+    private static class WithLogging implements Action<ExecutionListener> {
+        private final ServiceRegistry loggingServices;
+        private final List<String> args;
+        private final LoggingConfiguration loggingConfiguration;
+        private final Action<ExecutionListener> action;
+
+        WithLogging(ServiceRegistry loggingServices, List<String> args, LoggingConfiguration loggingConfiguration, Action<ExecutionListener> action) {
+            this.loggingServices = loggingServices;
+            this.args = args;
+            this.loggingConfiguration = loggingConfiguration;
+            this.action = action;
+        }
+
+        @Override
+        public void execute(ExecutionListener executionListener) {
+            final Action<Throwable> reporter = new BuildExceptionReporter(loggingServices.get(StyledTextOutputFactory.class), loggingConfiguration, clientMetaData());
+
+            BuildOptionBackedConverter<WelcomeMessageConfiguration> welcomeMessageConverter = new BuildOptionBackedConverter<>(new WelcomeMessageBuildOptions());
+            BuildOptionBackedConverter<LoggingConfiguration> loggingBuildOptions = new BuildOptionBackedConverter<>(new LoggingConfigurationBuildOptions());
+            InitialPropertiesConverter propertiesConverter = new InitialPropertiesConverter();
+            BuildLayoutConverter buildLayoutConverter = new BuildLayoutConverter();
+            LayoutToPropertiesConverter layoutToPropertiesConverter = new LayoutToPropertiesConverter(new BuildLayoutFactory());
+
+            BuildLayoutResult buildLayout = buildLayoutConverter.defaultValues();
+
+            CommandLineParser parser = new CommandLineParser();
+            propertiesConverter.configure(parser);
+            buildLayoutConverter.configure(parser);
+            loggingBuildOptions.configure(parser);
+
+            parser.allowUnknownOptions();
+            parser.allowMixedSubcommandsAndOptions();
+
+            WelcomeMessageConfiguration welcomeMessageConfiguration = new WelcomeMessageConfiguration(WelcomeMessageDisplayMode.ONCE);
+
+            try {
+                ParsedCommandLine parsedCommandLine = parser.parse(args);
+                InitialProperties initialProperties = propertiesConverter.convert(parsedCommandLine);
+
+                // Calculate build layout, for loading properties and other logging configuration
+                buildLayout = buildLayoutConverter.convert(initialProperties, parsedCommandLine, null);
+
+                // Read *.properties files
+                AllProperties properties = layoutToPropertiesConverter.convert(initialProperties, buildLayout);
+
+                // Calculate the logging configuration
+                loggingBuildOptions.convert(parsedCommandLine, properties, loggingConfiguration);
+
+                // Get configuration for showing the welcome message
+                welcomeMessageConverter.convert(parsedCommandLine, properties, welcomeMessageConfiguration);
+            } catch (CommandLineArgumentException e) {
+                // Ignore, deal with this problem later
+            }
+
+            LoggingManagerInternal loggingManager = loggingServices.getFactory(LoggingManagerInternal.class).create();
+            loggingManager.setLevelInternal(loggingConfiguration.getLogLevel());
+            loggingManager.start();
+
+            try {
+                Action<ExecutionListener> exceptionReportingAction =
+                    new ExceptionReportingAction(reporter, loggingManager,
+                        new NativeServicesInitializingAction(buildLayout, loggingConfiguration, loggingManager,
+                            new WelcomeMessageAction(buildLayout, welcomeMessageConfiguration,
+                                new DebugLoggerWarningAction(loggingConfiguration, action))));
+
+                exceptionReportingAction.execute(executionListener);
+            } finally {
+                loggingManager.stop();
+            }
+        }
+    }
+}
