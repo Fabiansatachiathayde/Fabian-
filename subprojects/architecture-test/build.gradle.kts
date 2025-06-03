9acc9988c476b78d648110793cc572e01d63cf07
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-02 23:15:54 -0500

    Initialize asodja/version-catalog-docs

---
+++ subprojects/architecture-test/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("gradlebuild.internal.java")
+    id("gradlebuild.binary-compatibility")
+}
+
+dependencies {
+    currentClasspath(project(":distributions-full"))
+    testImplementation(project(":base-services"))
+    testImplementation(project(":model-core"))
+    testImplementation(project(":file-temp"))
+    testImplementation(project(":core"))
+
+    testImplementation(libs.archunitJunit5)
+    testImplementation(libs.guava)
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.16.1")
+
+    testRuntimeOnly(project(":distributions-full"))
+}
+
+tasks.test {
+    // Looks like loading all the classes requires more than the default 512M
+    maxHeapSize = "900M"
+
+    // Only use one fork, so freezing doesn't have concurrency issues
+    maxParallelForks = 1
+
+    systemProperty("org.gradle.public.api.includes", gradlebuild.basics.PublicApi.includes.joinToString(":"))
+    systemProperty("org.gradle.public.api.excludes", gradlebuild.basics.PublicApi.excludes.joinToString(":"))
+    jvmArgumentProviders.add(ArchUnitFreezeConfiguration(
+        project.file("src/changes/archunit_store"),
+        providers.gradleProperty("archunitRefreeze").map { true })
+    )
+}
+
+class ArchUnitFreezeConfiguration(
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    val location: File,
+    @get:Optional
+    @get:Input
+    val refreeze: Provider<Boolean>
+) : CommandLineArgumentProvider {
+
+    override fun asArguments(): Iterable<String> {
+        val refreezeBoolean = refreeze.getOrElse(false)
+        return listOf(
+            "-Darchunit.freeze.store.default.path=${location.absolutePath}",
+            "-Darchunit.freeze.refreeze=${refreezeBoolean}",
+            "-Darchunit.freeze.store.default.allowStoreUpdate=${refreezeBoolean}"
+        )
+    }
+}
