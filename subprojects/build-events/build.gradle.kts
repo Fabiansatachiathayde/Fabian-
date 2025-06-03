35845695d0fb638c94da7ad44fec5a9d35d1be86
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-02 23:30:47 -0500

    Initialize jbartok/installation-properties

---
+++ subprojects/build-events/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("gradlebuild.distribution.api-java")
+}
+
+dependencies {
+    implementation(project(":base-services"))
+    implementation(project(":messaging"))
+    implementation(project(":core-api"))
+    implementation(project(":core"))
+    implementation(project(":model-core"))
+    implementation(project(":tooling-api"))
+
+    implementation(libs.jsr305)
+    implementation(libs.guava)
+
+    testImplementation(project(":internal-testing"))
+    testImplementation(project(":model-core"))
+
+    integTestImplementation(project(":logging")) {
+        because("This isn't declared as part of integtesting's API, but should be as logging's classes are in fact visible on the API")
+    }
+    integTestImplementation(project(":build-option"))
+    integTestImplementation(project(":enterprise-operations"))
+
+    integTestDistributionRuntimeOnly(project(":distributions-basics"))  {
+        because("Requires ':toolingApiBuilders': Event handlers are in the wrong place, and should live in this project")
+    }
+}
