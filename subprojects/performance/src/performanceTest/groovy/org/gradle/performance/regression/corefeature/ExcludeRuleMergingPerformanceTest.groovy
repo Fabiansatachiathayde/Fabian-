f22b6c0339b513bfea61052bb0b2c04f83b26b22
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-03 00:32:04 -0500

    Initialize jvandort/allow-artifacts-to-skip-metadata-fetch

---
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.performance.regression.corefeature
+
+import org.gradle.performance.AbstractCrossVersionPerformanceTest
+import org.gradle.performance.WithExternalRepository
+import org.gradle.performance.annotations.RunFor
+import org.gradle.performance.annotations.Scenario
+import spock.lang.Ignore
+
+import static org.gradle.performance.annotations.ScenarioType.PER_COMMIT
+import static org.gradle.performance.results.OperatingSystem.LINUX
+
+@RunFor(
+    @Scenario(type = PER_COMMIT, operatingSystems = [LINUX], testProjects = ["excludeRuleMergingBuild"])
+)
+@Ignore("SLG-temporarily ignore to build a new nightly")
+class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceTest implements WithExternalRepository {
+
+    def setup() {
+        runner.minimumBaseVersion = '5.6.4'
+        runner.targetVersions = ["7.5-20220206232035+0000"]
+    }
+
+    def "merge exclude rules"() {
+        startServer()
+
+        given:
+        runner.tasksToRun = ['resolveDependencies']
+        runner.args = ['-PuseHttp', "-PhttpPort=${serverPort}", "-Dorg.gradle.parallel=false"]
+
+        when:
+        def result = runner.run()
+
+        then:
+        result.assertCurrentVersionHasNotRegressed()
+
+        cleanup:
+        stopServer()
+    }
+
+    def "merge exclude rules (parallel)"() {
+        startServer()
+
+        given:
+        runner.tasksToRun = ['resolveDependencies']
+        runner.args = ['-PuseHttp', "-PhttpPort=${serverPort}", "--parallel"]
+        when:
+        def result = runner.run()
+
+        then:
+        result.assertCurrentVersionHasNotRegressed()
+
+        cleanup:
+        stopServer()
+    }
+}
