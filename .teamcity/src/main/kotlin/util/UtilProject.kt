b4cebdaaf7797f50f4fb1a093f9ad2af05d5e13f
Fabian Athayde <fabianathaydesr@gmail.com>
2025-06-03 00:39:10 -0500

    Initialize wolfs/watchfs/flaky-test

---
+++ .teamcity/src/main/kotlin/util/UtilProject.kt
@@ -0,0 +1,23 @@
+package util
+
+import common.Arch
+import common.BuildToolBuildJvmM1
+import common.Os
+import jetbrains.buildServer.configs.kotlin.v2019_2.Project
+
+object UtilProject : Project({
+    id("Util")
+    name = "Util"
+
+    buildType(RerunFlakyTest(Os.LINUX))
+    buildType(RerunFlakyTest(Os.WINDOWS))
+    buildType(RerunFlakyTest(Os.MACOS, Arch.AMD64))
+    buildType(RerunFlakyTest(Os.MACOS, Arch.AARCH64, BuildToolBuildJvmM1))
+    buildType(WarmupEc2Agent)
+
+    buildType(PublishKotlinDslPlugin)
+
+    params {
+        param("env.GRADLE_ENTERPRISE_ACCESS_KEY", "%ge.gradle.org.access.key%")
+    }
+})
